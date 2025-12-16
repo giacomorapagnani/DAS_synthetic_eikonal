6e3707fbf8681a05ac04f042f9a01db4f9765806
giacomorapagnani <g.rapagnani@studenti.unipi.it>
2025-12-16 15:08:28 +0100

    up

--- 3D/codes/3D_convolutional_synth.py
+++ 3D/codes/3D_convolutional_synth.py
@@ -14,9 +14,14 @@ class ConvolutionalSynth:
             Station_name    Latitude    Longitude   Elevation_km
     """
 
-    def __init__(self, events_path, fiber_geometry_path):
+    def __init__(self, events_path, fiber_geometry_path, 
+                 NLL_grid_parameters, NLL_matrices_parameters, 
+                 time_parameters):
         self.event = self._load_events(events_path)
         self.fiber_geometry = self._load_fiber_geometry(fiber_geometry_path)
+        self.tt_class = TravelTimeEvent(NLL_grid_parameters, fiber_geometry_path)
+        self._load_matrices_parameters(NLL_matrices_parameters)
+        self._load_time_parameters(time_parameters)
 
     def _radiation_patterns(self,phase):
         """
@@ -86,21 +91,43 @@ class ConvolutionalSynth:
         # !!!MISSING!!!
         return
 
-    def trace_amplitude(self,phase):
+    def trace_amplitude(self,phase, exclude_directivity=False,exclude_radiation_pattern=False):
         # Calculate trace amplitude at a given fiber's channel.
-        dir_fib = self._directivity_fiber(phase)
-        rad_pat = self._radiation_patterns(phase)
+        if exclude_directivity:
+            dir_fib = 1
+        else:
+            dir_fib = self._directivity_fiber(phase)
+        if exclude_radiation_pattern:
+            rad_pat = 1
+        else:
+            rad_pat = self._radiation_patterns(phase)
         amp = 1 * dir_fib * rad_pat
         return  amp
 
-    def gen_data_matrix(self, dt, time_window):
-        # Generates empty time vs channels data matrices for P and S waves
+    def _gen_data_matrix(self, dt, time_window):
         self.dt = dt
-        self.tax = self._gen_time_axis(time_window)
-        self.data_p=np.zeros((np.size(self.fiber_geometry),np.size(tax)))
-        self.data_s=np.zeros((np.size(self.fiber_geometry),np.size(tax)))
-        return
+        self.tax = self.__gen_time_axis(time_window)
+        data_p=np.zeros((np.size(self.fiber_geometry),np.size(tax)))
+        data_s=np.zeros((np.size(self.fiber_geometry),np.size(tax)))
+        return data_p, data_s
     
+    def __gen_time_axis(self,tmax):
+        ns=int(round(tmax/self.dt))
+        tax=np.arange(ns)*dt
+        return tax
+
+    def convolution(self,event):
+        # generates P and S matrices for a given event and convolves with wavelet
+        # event (list) : EventName OriginTime Latitude(deg) Longitude(deg)
+        #                Depth(km) Magnitude Strike Dip Rake
+        tt_p=self.tt_class.get_travel_time(event, tt_nll_p_miss)
+        tt_s=self.tt_class.get_travel_time(event, tt_nll_s_miss)
+
+        tt_p_indx= int( round( tt_p / self.dt ) )
+        tt_s_indx= int( round( tt_s / self.dt ) )
+
+        dataP,dataS=self._gen_data_matrix(self.dt, time_window)
+        return
     ######################################
     ######################################
     ######################################
@@ -111,32 +138,31 @@ class ConvolutionalSynth:
     ######################################
     ######################################
 
-    def _gen_time_axis(self,tmax):
-        ns=int(round(tmax/self.dt))
-        tax=np.arange(ns)*dt
-        return tax
-
-    def ricker(self,f0, nsamples, dt=None, derivative=False):
-        # if 'derivative=True' compute derivative of ricket wavelet
-        if dt is None:
+    def ricker(self):
+        # select dt or dt_w (if provided)
+        if self.dt_w is None:
             dt=self.dt  
-        if (nsamples%2):
-            t=((nsamples-1)*dt)/2.
         else:
-            nsamples +=1
-            t=((nsamples-1)*dt)/2.
-        x=f0*np.linspace(-t,t,nsamples)
+            dt=self.dt_w
+        # check if nsamples_w is even
+        if (self.nsamples_w%2):
+            t=((self.nsamples_w-1)*dt)/2.
+        else:
+            self.nsamples_w +=1
+            t=((self.nsamples_w-1)*dt)/2.
+        x=self.frequency_w*np.linspace(-t,t,self.nsamples_w)
         ric=( ( 1. - ( 2. * np.pi * (x**2.) ) ) * np.exp( -np.pi * (x**2.) ) )
-        tax=x/f0
-        if derivative:
+        tax=x/self.frequency_w
+        # if 'self.derivative_w=True' compute derivative of ricket wavelet
+        if self.derivative_w:
             # ric'
             ric[1:]= ( ric[1:]-ric[0:-1] ) / dt
             ric[0]=0
             # normalization
             w_max = np.max(np.abs(ric))
             ric=ric/w_max
-        self.ricker=ric
-        self.ricker_tax=tax
+        self.ricker_w=ric
+        self.ricker_w_tax=tax
         return
     
     def _load_fiber_geometry(self, filepath):
@@ -159,15 +185,32 @@ class ConvolutionalSynth:
                 events.append([event_name, str(tor), float(lat), float(lon), float(depth), 
                                float(mag), float(strike), float(dip), float(rake)])
         return events
+    
+    def _load_matrices_parameters(self,NLL_matrices_parameters):
+        db_path = NLL_matrices_parameters['db_path']
+        hdr_filename = NLL_matrices_parameters['hdr_filename']
+        precision = NLL_matrices_parameters['precision']
+        model = NLL_matrices_parameters['model']
+        # Load NLL traveltime matrices
+        tt_nll_class = Traveltimes(db_path, hdr_filename)
+        self.tt_nll_p = tt_nll_class.load_traveltimes('P', model, precision)
+        self.tt_nll_s = tt_nll_class.load_traveltimes('S', model, precision)
+        return
+    
+    def _load_time_parameters(self,time_parameters):
+        self.dt = time_parameters['dt']
+        self.time_window = time_parameters['time_window']
+        self.frequency_w = time_parameters['frequency_w']
+        self.nsamples_w = time_parameters['nsamples_w']
+        self.dt_w = time_parameters['dt_w']
+        self.derivative_w = time_parameters['derivative_w']
+        return
 
 if __name__ == "__main__":
     #%% INPUTS:
     workdir='../'
-    #### 1 - FIBER GEOMETRY
-    fiber_geometry_dir=os.path.join(workdir,'FIBER_GEOMETRY')
-    fiber_geometry_file=os.path.join(fiber_geometry_dir, 'flegrei_stations_geometry.txt')      ### CHANGE ###
 
-    #### 2 - EVENTS
+    #### 1 - EVENTS (load or generate)
     SWITCH_generate_new_catalogue=False
     if SWITCH_generate_new_catalogue:
         # generate new synthetic catalogue
@@ -204,8 +247,12 @@ if __name__ == "__main__":
         print(f'READING EVENTS FROM FILE: {filename_events}')
         events_file=os.path.join(event_dir,filename_events)
 
-    #### 3 - NLL grid parameters 
-    # (CHANGE: should match those used to generate the traveltimes)
+    #### 2 - FIBER GEOMETRY (load)
+    fiber_geometry_dir=os.path.join(workdir,'FIBER_GEOMETRY')
+    fiber_geometry_file=os.path.join(fiber_geometry_dir, 'flegrei_stations_geometry.txt')      ### CHANGE ###
+
+    #### 3 - NLL grid parameters (insert)
+    # (CHANGE: must match those used to generate the traveltimes)
     nx=151 # number of grid points in x direction
     ny=151 # number of grid points in y direction
     nz=61  # number of grid points in z direction
@@ -218,40 +265,45 @@ if __name__ == "__main__":
     coord_origin_lat=40.777242 # latitude of the grid origin 
     coord_origin_lon=14.025848 # longitude of the grid origin
     coord_origin_ele=0.0       # elevation of the grid origin (km)
-    NLL_grid_parameters = {
+    NLL_grid_inputs = {
         'nx': nx,'ny': ny, 'nz': nz, 'dx': dx, 'dy': dy, 'dz': dz,
         'co_x': ox, 'co_y': oy, 'co_z': oz, 
         'co_lat': coord_origin_lat, 'co_lon': coord_origin_lon, 'co_ele': coord_origin_ele}
 
-    #### 4 - NLL traveltime matrices
+    #### 4 - NLL traveltime matrices (load)
     db_path = '../NLL/FLEGREI/nll_grid'                                                    ### CHANGE ###
     hdr_filename = 'header.hdr'
     precision='single'
     model = 'time'
-    tt_nll_obj = Traveltimes(db_path, hdr_filename)
-    tt_nll_p = tt_nll_obj.load_traveltimes('P', model, precision)
-    tt_nll_s = tt_nll_obj.load_traveltimes('S', model, precision)
+    NLL_matrices_inputs = {
+        'db_path': db_path,
+        'hdr_filename': hdr_filename,
+        'precision': precision,
+        'model': model}
 
-    #### 5 - Time axis
+    #### 5 - TIME AXIS (generate)
     dt= 0.02                                        # ARBITRARY (?) 
     time_window= 120 #s                             # ARBITRARY (?)
-    tax=time_win
 
-    #### 6 - Ricker wavelet
-    nsamples_w=200                                  # CHANGE
+    #### 6 - RICKER WAVELET (generate)
     frequency_w=75                                  # CHANGE
-    wavelet,taxw=ricker()
+    nsamples_w=200                                  # CHANGE
+    dt_w=None                                       # if None, use dt
+    derivative_w=False                              # if True, use derivative of Ricker
+
+    time_inputs={
+        'dt':dt, 'time_window':time_window,
+        'frequency_w':frequency_w, 'nsamples_w':nsamples_w ,
+        'dt_w':dt_w, 'derivative_w':derivative_w}
 
     #%% SYNTHETIC GENERATION:
-    # TravelTimeEvent class
-    tt_class=TravelTimeEvent(NLL_grid_parameters= NLL_grid_parameters,
-                             fiber_geometry_path=fiber_geometry_file)
 
     # ConvolutionalSynth class
-    synth=ConvolutionalSynth(events_path=events_file, 
-                             fiber_geometry_path=fiber_geometry_file)
-    tt_p=tt_class.get_travel_time(event, tt_nll_p)
-    tt_s=tt_class.get_travel_time(event, tt_nll_s)
+    synth=ConvolutionalSynth(events_path = events_file, # 1 - EVENTS
+                             fiber_geometry_path = fiber_geometry_file, # 2 - FIBER GEOMETRY
+                             NLL_grid_parameters = NLL_grid_inputs, # 3 - NLL GRID PARAMETERS
+                             NLL_matrices_parameters = NLL_matrices_inputs, # 4 - NLL MATRICES
+                             time_parameters = time_inputs) # 5,6 - TIME AXIS + RICKER WAVELET
 
     #----------------------------------------------------------------------
 
